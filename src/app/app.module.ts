a37d939a7582541a99c7a5481d09f9e22e1b21f8
David Fountain <savsin64@gmail.com>
2018-03-12 06:51:13 -0700

    Initial Commit + Created Homepage component, Router Module, base App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+  	AppComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+  selector: 'home',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent {
+  title = 'Freelance Bootcamp Dashboard';
+}
